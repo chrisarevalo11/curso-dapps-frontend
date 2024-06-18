46fa4f43df91bc4fa3718e4d303b8d2ae4d91aba
chrisarevalo11 <christian.arevalo.fonseca@gmail.com>
2024-06-18 00:23:20 -0500

    ✨ Implemented wagmi, rk and faucet

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,88 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useAccount, useReadContract, useWriteContract } from 'wagmi'
+import { CONTRACT_ADDRESS } from './constants'
+import { abi } from './assets/abis/erc20'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { waitForTransactionReceipt } from 'wagmi/actions'
+import { config } from './main'
+import { toast } from 'react-toastify'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(): JSX.Element {
+  const { address, isConnected } = useAccount()
+  const [isMinting, setIsMinting] = useState(false)
+
+  const { data, isLoading, refetch } = useReadContract({
+    abi,
+    address: CONTRACT_ADDRESS,
+    functionName: 'balanceOf',
+    args: [address],
+  })
+
+  const { writeContractAsync } = useWriteContract()
+
+  const handleMint = async () => {
+    setIsMinting(true)
+    try {
+      const txHash = await writeContractAsync({
+        abi,
+        address: CONTRACT_ADDRESS,
+        functionName: 'mint',
+        args: [address, 100],
+      })
+
+      await waitForTransactionReceipt(config, {
+        confirmations: 1,
+        hash: txHash,
+      })
+
+      setIsMinting(false)
+      toast('Minted successfully')
+
+      refetch()
+    } catch (error) {
+      toast.error('Error while minting. Try again.')
+      setIsMinting(false)
+      console.error(error)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-center w-full">
+      <section className="space-y-5">
+        <h1 className="text-4xl font-bold text-center">
+          🚀 DAPP Token Faucet 🚀
+        </h1>
+        <div className="p-4 border border-zinc-700 flex flex-col gap-5 items-center rounded-xl">
+          <ConnectButton showBalance={false} accountStatus={'avatar'} />
+          {!isConnected ? (
+            <>
+              <h2>First make sure your wallet is connected</h2>
+            </>
+          ) : (
+            <div className="flex flex-col gap-5 items-center">
+              <p className="text-xl  text-center">
+                📇 <span className="font-bold">Address:</span> {address}
+              </p>
+              <p className="text-xl  text-center">
+                💰 <span className="font-bold">Balance (DAPP):</span>{' '}
+                {isLoading ? (
+                  <span className="opacity-50">loading...</span>
+                ) : (
+                  data?.toString()
+                )}
+              </p>
+              <button
+                className="py-1 px-3 bg-zinc-800 rounded-lg hover:scale-105 transition-all disabled:opacity-50"
+                onClick={handleMint}
+                disabled={isMinting}
+              >
+                {isMinting ? 'Minting...' : 'Mint'}
+              </button>
+            </div>
+          )}
+        </div>
+      </section>
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  RainbowKitProvider,
+  darkTheme,
+  getDefaultConfig,
+} from '@rainbow-me/rainbowkit'
+import { arbitrum, arbitrumSepolia } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const queryClient = new QueryClient()
+
+export const config = getDefaultConfig({
+  appName: 'Curso dApps on-chain',
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  chains: [arbitrumSepolia, arbitrum],
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={darkTheme()}>
+          <App />
+          <ToastContainer />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>
 )
